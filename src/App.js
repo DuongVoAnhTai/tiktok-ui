f29a56b1ef2b86ad920001aebea38f95cdb74488
duongtai <theshy5893@gmail.com>
2025-07-09 16:03:04 +0700

    feat: css header and content page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { Fragment } from 'react';
-import { DefaultLayout } from './components/Layout';
+import { DefaultLayout } from './components/Layouts';
 
 function App() {
     return (
@@ -9,12 +9,11 @@ function App() {
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
-                        let Layout = DefaultLayout
-                        if(route.layout) {
-                            Layout = route.layout
-                        }
-                        else if (route.layout === null) {
-                            Layout = Fragment
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
                         }
 
                         const Page = route.component;
