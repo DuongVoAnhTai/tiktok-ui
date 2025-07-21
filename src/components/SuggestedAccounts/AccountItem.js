48a42794bc31014fb0d3e10e5c82afb22feaeec9
duongtai <theshy5893@gmail.com>
2025-07-21 18:21:39 +0700

    feat: account sidebar

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import styles from './SuggestedAccounts.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('account-item')}>
+            <img
+            className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/281d685416feacaa3dd63703b4b28690~tplv-tiktokx-cropcenter:100:100.jpeg?dr=14579&refresh_token=922958c2&x-expires=1753264800&x-signature=zdpImdVwAch2vBZJ9hudxVvbbWo%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"
+                alt=""
+            />
+
+            <div className={cx('item-info')}>
+                <p className={cx('nickname')}>
+                    <strong>quocnguyenphu</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Quốc Nguyễn Phú</p>
+            </div>
+        </div>
+        
+    );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -3,6 +3,7 @@ import styles from './Sidebar.module.scss';
 import config from '~/config';
 import Menu, { MenuItem } from './Menu';
 import { HomeActiveIcon, HomeIcon, LiveActiveIcon, LiveIcon, UserGroupActiveIcon, UserGroupIcon } from '~/components/Icons';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
 
 const cx = classNames.bind(styles);
 
@@ -14,6 +15,8 @@ function Sidebar() {
                 <MenuItem title='Following' to={config.routes.following} icon={<UserGroupIcon />} activeIcon={<UserGroupActiveIcon />} />
                 <MenuItem title='LIVE' to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
             </Menu>
+            <SuggestedAccounts label='Suggested accounts' />
+            <SuggestedAccounts label='Following accounts' />
         </aside>
     );
 }
